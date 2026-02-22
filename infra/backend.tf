3a92b09f4d4735cf7bad21baafccac0be8884c08
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-02-22 19:48:26 -0300

    fix: bucket moved to pipeline files

--- infra/backend.tf
+++ infra/backend.tf
@@ -10,16 +10,3 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "artifacts" {
-  bucket = "micropizzeria-artifacts"
-
-  tags = var.common_tags
-}
-
-resource "aws_s3_bucket_versioning" "artifacts" {
-  bucket = aws_s3_bucket.artifacts.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
