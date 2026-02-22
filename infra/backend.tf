913a79cbf82f13ae6fc0dda5cd19cc348ea36b08
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-02-22 19:45:22 -0300

    fix: artifacts bucket added

--- infra/backend.tf
+++ infra/backend.tf
@@ -9,3 +9,17 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "micropizzeria-artifacts"
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_versioning" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
