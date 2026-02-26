b4c79a604d40f9728fd8ef28ea9198e58beaee9c
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-02-26 09:20:56 -0300

    fix: ensure ECR repository is created only if it doesn't exist

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,4 +1,10 @@
+# The repository resource will be created only if it doesn't exist already. 
+data "aws_ecr_repository" "existing" {
+  name = "micropizzeria-app"
+}
+
 resource "aws_ecr_repository" "api_repo" {
+  count = length(try(data.aws_ecr_repository.existing.repository_url, "")) == 0 ? 1 : 0
   name = "micropizzeria-app"
   image_tag_mutability = "MUTABLE" # allows to overwrite an image using the same tag
   force_delete = true
