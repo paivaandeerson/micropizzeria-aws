bfe8c92a25169623e4c977b6151ded0f3490b862
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-03-03 10:03:18 -0300

    fix: change to get token from ECR and download the image

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,15 +1,38 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 7.0"
 
   name = "main-vpc"
   cidr = "10.0.0.0/16"
 
-  # alb requires at least 2 AZs, so we need to specify them explicitly
   azs = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = false
+  
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_vpc_endpoints = true
+
+  vpc_endpoints = {
+    ecr_api = {
+      service             = "ecr.api"
+      subnet_ids          = module.vpc.private_subnets
+      private_dns_enabled = true
+    }
+
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      subnet_ids          = module.vpc.private_subnets
+      private_dns_enabled = true
+    }
+
+    s3 = {
+      service = "s3"
+    }
+  }
+
   tags = var.common_tags
 }
