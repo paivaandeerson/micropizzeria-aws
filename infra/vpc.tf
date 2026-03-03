36c1e52dfb4b1a38cc2b54b8686871cfa72a785a
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-03-03 10:12:54 -0300

    feat: vpc_endpoints, ecr and sg added

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,38 +1,59 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.0"
+  version = "~> 5.0"
 
   name = "main-vpc"
   cidr = "10.0.0.0/16"
 
+  # alb requires at least 2 AZs, so we need to specify them explicitly
   azs = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = false
-  
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  tags = var.common_tags
+}
 
-  enable_vpc_endpoints = true
+#vpc_endpoints
+resource "aws_security_group" "vpc_endpoints" {
+  vpc_id = module.vpc.vpc_id
 
-  vpc_endpoints = {
-    ecr_api = {
-      service             = "ecr.api"
-      subnet_ids          = module.vpc.private_subnets
-      private_dns_enabled = true
-    }
-
-    ecr_dkr = {
-      service             = "ecr.dkr"
-      subnet_ids          = module.vpc.private_subnets
-      private_dns_enabled = true
-    }
-
-    s3 = {
-      service = "s3"
-    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
   }
 
-  tags = var.common_tags
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.us-east-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.us-east-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.us-east-1.s3"
+
+  route_table_ids = module.vpc.private_route_table_ids
 }
