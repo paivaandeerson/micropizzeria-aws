08a797ca95642dc9d2749cdf3cd06696f5fe4cd8
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-03-05 12:29:54 -0300

    fix: removing duplicated content

--- infra/payment-ecs.tf
+++ infra/payment-ecs.tf
@@ -68,24 +68,6 @@ module "ecs" {
 
       }
     }
-     xray-daemon = {
-       image = "public.ecr.aws/xray/aws-xray-daemon:latest"    
-
-       launch_type = "FARGATE"
-     
-       subnet_ids = module.vpc.private_subnets
-     
-       security_group_ids = [aws_security_group.ecs_tasks.id]
-       cpu    = 32
-       memory = 256
-    
-       port_mappings = [
-         {
-           containerPort = 2000
-           protocol      = "udp"
-         }
-       ]
-     }
   }
 
   tags = var.common_tags
@@ -110,7 +92,29 @@ resource "aws_security_group" "ecs_tasks" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "xray" {
-  role       = "ecsTaskExecutionRole"
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
 }
