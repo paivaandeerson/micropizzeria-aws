5bb99444a4bc0c88ad6215cdf2b4659ed89f420d
Anderson Paiva <anderson.ultimato@hotmail.com>
2026-03-06 07:55:29 -0300

    fix: ecr provisioned by terraform

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,15 +1,11 @@
-# The repository resource will be created only if it doesn't exist already. 
-data "aws_ecr_repository" "existing" {
-  name = "micropizzeria-app"
-}
-
 resource "aws_ecr_repository" "api_repo" {
-  count = length(try(data.aws_ecr_repository.existing.repository_url, "")) == 0 ? 1 : 0
-  name = "micropizzeria-app"
-  image_tag_mutability = "MUTABLE" # allows to overwrite an image using the same tag
-  force_delete = true
+  name                 = "micropizzeria-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  tags = var.common_tags
 }
 
 output "ecr_repository_url" {
-  value = length(aws_ecr_repository.api_repo) > 0 ? aws_ecr_repository.api_repo[0].repository_url : ""
+  value = aws_ecr_repository.api_repo.repository_url
 }
